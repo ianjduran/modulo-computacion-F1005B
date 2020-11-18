eca6a0022be131a7715cf7fc77cb44d8e3815866
Ian <ianjduran@gmail.com>
2020-11-18 13:23:34 -0400

    correcciones

--- tarea1/areaBajoCurva.m
+++ tarea1/areaBajoCurva.m
@@ -3,7 +3,7 @@ ecuacion = x^2;
 
 start = 0;
 finish = 10;
-step = .01;
+step = .5;
 
 %diferencia en x = steps = base
 
@@ -11,16 +11,16 @@ puntos_x = start:step:finish;
 area_acumulada = 0;
 for i = 2:length(puntos_x)
     % disp("area acumulada: " + num2str(area_acumulada))
-    alturaPequenia = obtenerAltura(ecuacion, puntos_x(i-1));
-    alturaGrande = obtenerAltura(ecuacion, puntos_x(i));
-    area_trapecio = obtenerAreaTrapecio(alturaPequenia, alturaGrande, step);
+    area_trapecio = obtenerAreaTrapecio(ecuacion ,puntos_x(i-1), puntos_x(i), step);
     area_acumulada = area_acumulada + area_trapecio;
 end
 
 disp(area_acumulada);
 
-function surface = obtenerAreaTrapecio(small_height, tall_height, base)
-    surface = base*(small_height + tall_height)/2;
+function surface = obtenerAreaTrapecio(eqn, punto_inicial, punto_final, base)
+    small_height = obtenerAltura(eqn, punto_inicial);
+    big_height = obtenerAltura(eqn, punto_final);
+    surface = base*(small_height + big_height)/2;
 end
 
 function altura = obtenerAltura(eqn, punto)
